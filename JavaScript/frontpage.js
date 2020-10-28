bf94c4aeb5c81877c42cf603342b0abaab12ca00
ZeyuDaMeme <zeyu.shao@hotmail.com>
2020-10-27 21:59:29 -0400

    NAV FUN FIX

---
+++ JavaScript/frontpage.js
@@ -0,0 +1,18 @@
+
+/*
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    document.getElementById("nav").style.padding = "0vw 0vw";
+    document.getElementById("logo-static").style.width = "50px";
+    document.getElementById("logo-text-static").style.fontSize = "25px";
+    document.getElementById("navA").style.fontSize = "15px";
+  } else {
+    document.getElementById("nav").style.padding = "2.5vw 0vw";
+    document.getElementById("logo-static").style.width = "70px";
+    document.getElementById("logo-text-static").style.fontSize = "40px";
+    document.getElementById("navA").style.fontSize = "30px";
+  }
+}
+*/
